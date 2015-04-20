b40e9c7029d21e9205c6a9e0e570536c1fcb0d85
mimslee <sl2578@cornell.edu>
2015-04-20 16:21:38 -0400

    final

---
+++ main.py
@@ -0,0 +1,68 @@
+import sqlite3, datetime
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+
+     
+# CONFIG DATABASE
+DATABASE = '/tmp/todo.db'
+DEBUG = True
+SECRET_KEY = 'pst_key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+# SETTING UP THE DB
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('todo.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+# ROUTES
+# Index page gets passed on information about tasks
+@app.route("/")
+def index():
+    cur = g.db.execute('select id, title, descr, done, dline, finish from entries order by id desc')
+    tasks = [dict(id=row[0], title=row[1], descr=row[2], done=row[3], dline=row[4], finish=row[5]) for row in cur.fetchall()]
+    return render_template('index.html', tasks=tasks)
+
+# Adds new task to list and stores relevant information
+@app.route('/add', methods=['POST'])
+def add_entry():
+    g.db.execute('insert into entries (title, descr, done, dline, finish) values (?, ?, ?, ?, ?)', [request.form['title'], request.form['descr'], 0, request.form['dline'], str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))])
+    g.db.commit()
+    return redirect(url_for('index'))
+
+# Deletes task from either lists
+@app.route('/delete', methods=['POST'])
+def delete_task():
+    g.db.execute('delete from entries where id = ?', [request.form['task_to_delete']])
+    g.db.commit()
+    return redirect(url_for('index'))
+
+# Moves task from unfinished to finished
+@app.route('/move', methods=['POST'])
+def move_task():
+    g.db.execute('update entries set done=1 where id=?', [request.form['task_to_move']])
+    g.db.commit()
+    return redirect(url_for('index'))
+
+# INITIALIZATION
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ todo.sql
@@ -0,0 +1,9 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  descr text not null,
+  done integer not null,
+  dline datetime,
+  finish text not null
+);
