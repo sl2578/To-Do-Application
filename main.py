d03ed33bf158f26077cb8a054ad72bbd9788f135
sl2578 <sl2578@cornell.edu>
2015-06-17 18:52:20 +0000

    modified to work with mysql database

--- main.py
+++ main.py
@@ -1,68 +1,79 @@
-import sqlite3, datetime
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import datetime
+from flask import Flask, json, request, session, redirect, url_for, abort, render_template, flash
 from contextlib import closing
-
-     
-# CONFIG DATABASE
-DATABASE = '/tmp/todo.db'
-DEBUG = True
-SECRET_KEY = 'pst_key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-
-
-# SETTING UP THE DB
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('todo.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
+app.config['MYSQL_DATABASE_USER'] = 'user1'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'tsevwh'
+app.config['MYSQL_DATABASE_DB'] = 'tododb'
+app.config['MYSQL_DATABASE_HOST'] = '23.253.70.25'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+db = MySQL()
+db.init_app(app)
 
 # ROUTES
 # Index page gets passed on information about tasks
 @app.route("/")
 def index():
-    cur = g.db.execute('select id, title, descr, done, dline, finish from entries order by id desc')
-    tasks = [dict(id=row[0], title=row[1], descr=row[2], done=row[3], dline=row[4], finish=row[5]) for row in cur.fetchall()]
+    conn = db.connect()
+    cursor = conn.cursor()
+    cursor.callproc('get_table', ())
+    tasks = [dict(id=row[0], title=row[1], descr=row[2], done=row[3], dline=row[4], finish=row[5]) for row in cursor.fetchall()]
+    cursor.close() 
+    conn.close()
     return render_template('index.html', tasks=tasks)
+
 
 # Adds new task to list and stores relevant information
 @app.route('/add', methods=['POST'])
 def add_entry():
-    g.db.execute('insert into entries (title, descr, done, dline, finish) values (?, ?, ?, ?, ?)', [request.form['title'], request.form['descr'], 0, request.form['dline'], str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))])
-    g.db.commit()
-    return redirect(url_for('index'))
+    _title = request.form['title']
+    _descr = request.form['descr']
+    _dline = request.form['dline']
+    _done = 0
+
+    if _title and _descr and _dline:
+        conn = db.connect()
+        cursor = conn.cursor()
+        cursor.callproc('add_task', (_title, _descr, _done, _dline))
+        data = cursor.fetchall()
+        if len(data) is 0:
+            conn.commit()
+            cursor.close() 
+            conn.close()  
+            return redirect(url_for('index'))
+        else: 
+            return json.dumps({'error':str(data[0])})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
 
 # Deletes task from either lists
-@app.route('/delete', methods=['POST'])
+@app.route('/delete', methods=['POST']) 
 def delete_task():
-    g.db.execute('delete from entries where id = ?', [request.form['task_to_delete']])
-    g.db.commit()
+    _id = request.form['task_to_delete']
+    conn = db.connect()
+    cursor = conn.cursor()
+    cursor.callproc('delete_task', (_id))
+    conn.commit()
+    cursor.close() 
+    conn.close()    
     return redirect(url_for('index'))
 
 # Moves task from unfinished to finished
 @app.route('/move', methods=['POST'])
 def move_task():
-    g.db.execute('update entries set done=1 where id=?', [request.form['task_to_move']])
-    g.db.commit()
+    _id = request.form['task_to_move']
+    _finish = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M'))
+    conn = db.connect()
+    cursor = conn.cursor()
+    cursor.callproc('move_task', (_id, _finish))
+    conn.commit()
+    cursor.close() 
+    conn.close()
     return redirect(url_for('index'))
 
 # INITIALIZATION
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
--- todo.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title text not null,
-  descr text not null,
-  done integer not null,
-  dline datetime,
-  finish text not null
-);
